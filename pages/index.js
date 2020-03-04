ad5e8b134bdf4126e39338b8899b127f3f0bf748
Moe <mohamed.amadou159@gmail.com>
2020-03-04 00:14:03 -0500

    Add Next-sass styling and header file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import App from "../src/App";
+import IndexHead from "./heads/index";
 import "../src/scss/main.scss";
 
 export default function Index() {
-  return <App />;
+  return (
+    <div>
+      <IndexHead />
+      <App />
+    </div>
+  );
 }
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import "./scss/main.scss";
 
 function App() {
   return (
