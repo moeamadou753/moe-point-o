78b70ad8b4f3e45f9fec48ccec4bde71414dad0b
Moe <mohamed.amadou159@gmail.com>
2020-03-09 02:48:13 -0400

    Add floating gradient orbs

--- src/App.js
+++ src/App.js
@@ -21,21 +21,38 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <div className="app-content__container">
-          <section className="landing"></section>
-
-          <section className="about-me">
-            <div className="h2">About Me</div>
-            <div className="about-me__description">
-              I am a double degree student in Computer Science and Business at
-              the University of Waterloo in Ontario, Canada. I’ve previously
-              done internships at TD Bank as an Android Developer and at
-              Accenture as a general Software Engineer. I am currently looking
-              for Fall 2020 (September - December) internship opportunities.
-              Please check out some of my links and work on this website and
-              leave me a message to connect and discuss opportunities or one of
-              my passions.
+          <section className="landing">
+            <span className="left-glowing-orb" />
+            <span className="right-glowing-orb" />
+            <div className="greeting">
+              <div className="greeting__title">
+                <span style={{ "font-weight": "normal" }}>👋🏿 Hello!</span> I’m
+                Moe.
+              </div>
+              <div className="greeting__body">
+                <span style={{ color: "#FFD600", "font-weight": "bold" }}>
+                  Thank you{" "}
+                </span>
+                for stopping by. Please take a look around to find out more
+                about my work, what inspires me, and my goals — I look forward
+                to connecting with you soon.
+              </div>
             </div>
           </section>
+
+          {/*<section className="about-me">*/}
+          {/*  <div className="h2">About Me</div>*/}
+          {/*  <div className="about-me__description">*/}
+          {/*    I am a double degree student in Computer Science and Business at*/}
+          {/*    the University of Waterloo in Ontario, Canada. I’ve previously*/}
+          {/*    done internships at TD Bank as an Android Developer and at*/}
+          {/*    Accenture as a general Software Engineer. I am currently looking*/}
+          {/*    for Fall 2020 (September - December) internship opportunities.*/}
+          {/*    Please check out some of my links and work on this website and*/}
+          {/*    leave me a message to connect and discuss opportunities or one of*/}
+          {/*    my passions.*/}
+          {/*  </div>*/}
+          {/*</section>*/}
         </div>
         <NavigationBar />
       </React.Fragment>
