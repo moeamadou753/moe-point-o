712742786a896ec9bd90b9957bf72cfde1bb207a
Moe <mohamed.amadou159@gmail.com>
2020-03-08 23:00:43 -0400

    Fix render function

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
     this.setState({ landingCardHover: false });
   };
 
+  render() {
     return (
       <div className="app-content__container">
         <section className="landing">
