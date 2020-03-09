81e6b2a5e960e042d2e3feaf9d96d29b6a895fbe
Moe <mohamed.amadou159@gmail.com>
2020-03-09 02:48:32 -0400

    Open portfolio links in new tab

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -17,35 +17,37 @@ class NavigationBar extends React.Component {
     return (
       <div className="nav-bar">
         <Link href="https://www.github.com/moeamadou753" replace>
-          <a className="nav-bar__icon">
+          <a className="nav-bar__icon" target="_blank">
             <SVG src={github} />
           </a>
         </Link>
         <Link href="https://www.linkedin.com/in/mohamed-amadou" replace>
-          <a className="nav-bar__icon">
+          <a className="nav-bar__icon" target="_blank">
             <SVG src={linkedin} />
           </a>
         </Link>
         <Link href="https://www.devpost.com/MohamedAmadou" replace>
-          <a className="nav-bar__icon">
+          <a className="nav-bar__icon" target="_blank">
             <SVG src={devpost} />
           </a>
         </Link>
         <Link href="https://www.twitter.com/Amadou159Amadou" replace>
-          <a className="nav-bar__icon">
+          <a className="nav-bar__icon" target="_blank">
             <SVG src={twitter} />
           </a>
         </Link>
-        <Link href="/about-me" replace>
+        <Link href="/" replace>
           <a className="nav-bar__text">ABOUT ME</a>
         </Link>
         <Link href={`/resumes/${resumeVer}.pdf`} replace>
-          <a className="nav-bar__text">RESUME</a>
+          <a className="nav-bar__text" target="_blank">
+            RESUME
+          </a>
         </Link>
-        <Link href="/symphonia" replace>
+        <Link href="/" replace>
           <a className="nav-bar__text">PROJECT SYMPHONIA</a>
         </Link>
-        <Link href="/contact" replace>
+        <Link href="/" replace>
           <a className="nav-bar__text-outline">
             <div className="nav-bar__text">CONTACT ME</div>
           </a>
