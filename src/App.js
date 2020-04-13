037f7438a447e40cd1d22317ba02e63cc79b0550
GitHub <mohamed.amadou159@gmail.com>
2020-04-13 19:00:46 -0400

    Add about me section

--- src/App.js
+++ src/App.js
@@ -49,19 +49,19 @@ class App extends React.Component {
             </div>
           </section>
 
-          {/*<section className="about-me">*/}
-          {/*  <div className="h2">About Me</div>*/}
-          {/*  <div className="about-me__description">*/}
-          {/*    I am a double degree student in Computer Science and Business at*/}
-          {/*    the University of Waterloo in Ontario, Canada. I’ve previously*/}
-          {/*    done internships at TD Bank as an Android Developer and at*/}
-          {/*    Accenture as a general Software Engineer. I am currently looking*/}
-          {/*    for Fall 2020 (September - December) internship opportunities.*/}
-          {/*    Please check out some of my links and work on this website and*/}
-          {/*    leave me a message to connect and discuss opportunities or one of*/}
-          {/*    my passions.*/}
-          {/*  </div>*/}
-          {/*</section>*/}
+          <section className="about-me">
+            <div className="h2">About Me</div>
+            <div className="about-me__description">
+              I am a double degree student in Computer Science and Business at
+              the University of Waterloo in Ontario, Canada. I’ve previously
+              done internships at TD Bank as an Android Developer and at
+              Accenture as a general Software Engineer. I am currently looking
+              for Fall 2020 (September - December) internship opportunities.
+              Please check out some of my links and work on this website and
+              leave me a message to connect and discuss opportunities or one of
+              my passions.
+            </div>
+          </section>
         </div>
         <NavigationBar />
       </React.Fragment>
