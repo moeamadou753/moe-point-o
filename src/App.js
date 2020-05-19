94d1b115a7e11346a0c1881fc23053d2742c87f6
GitHub <mohamed.amadou159@gmail.com>
2020-05-18 22:49:54 -0400

    Abstract out landing page

--- src/App.js
+++ src/App.js
@@ -3,63 +3,29 @@ import NavigationBar from "./components/NavigationBar";
 import "./scss/main.scss";
 import boysShot from "../src/assets/pics/action-shot-boys.png";
 import normalShot from "../src/assets/pics/normal-1.png";
+import LandingPage from "./components/LandingPage";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { landingCardHover: false };
   }
-
-  handleHover = () => {
-    this.setState({ landingCardHover: true });
-  };
-
-  handleLeave = () => {
-    this.setState({ landingCardHover: false });
-  };
 
   render() {
     return (
       <React.Fragment>
         <div className="app-content__container">
-          <section className="landing">
-            <span className="left-glowing-orb" />
-            <span className="right-glowing-orb" />
-
-            <div className="greeting">
-              <div className="greeting__left">
-                <img src={normalShot} className="greeting__pic" />
-              </div>
-
-              <div className="greeting__right">
-                <div className="greeting__title">
-                  <span style={{ fontWeight: "normal" }}>👋🏿 Hello!</span> I’m
-                  Moe.
-                </div>
-                <div className="greeting__body">
-                  <span style={{ color: "#FFD600", fontWeight: "bold" }}>
-                    Thank you{" "}
-                  </span>
-                  for stopping by. Please take a look around to find out more
-                  about my work, what inspires me, and my goals — I look forward
-                  to connecting with you soon.
-                </div>
-              </div>
-            </div>
-          </section>
-
+          <LandingPage />
           <section className="about-me">
-            <div className="h2">About Me</div>
+            <div className="h2"> About Me </div>
             <div className="about-me__description">
               I am a double degree student in Computer Science and Business at
-              the University of Waterloo in Ontario, Canada. I’ve previously
+              the University of Waterloo in Ontario, Canada.I’ ve previously
               done internships at TD Bank as an Android Developer and at
-              Accenture as a general Software Engineer. I am currently looking
-              for Fall 2020 (September - December) internship opportunities.
-              Please check out some of my links and work on this website and
-              leave me a message to connect and discuss opportunities or one of
-              my passions.
+              Accenture as a general Software Engineer.I am currently looking
+              for Fall 2020(September - December) internship
+              opportunities.Please check out some of my links and work on this
+              website and leave me a message to connect and discuss
+              opportunities or one of my passions.
             </div>
           </section>
         </div>
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <section className="landing">
+      <span className="left-glowing-orb" />
+      <span className="right-glowing-orb" />
+      <div className="greeting">
+        <div className="greeting__left">
+          <img src={normalShot} className="greeting__pic" />
+        </div>
+        <div className="greeting__right">
+          <div className="greeting__title">
+            <span
+              style={{
+                fontWeight: "normal",
+              }}
+            >
+              👋🏿Hello!
+            </span>
+            I’m Moe.
+          </div>
+          <div className="greeting__body">
+            <span
+              style={{
+                color: "#FFD600",
+                fontWeight: "bold",
+              }}
+            >
+              Thank you
+            </span>
+            for stopping by.Please take a look around to find out more about my
+            work, what inspires me, and my goals— I look forward to connecting
+            with you soon.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
