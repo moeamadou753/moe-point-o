34269525e39a42839af5088d3b9fb469eba3423b
GitHub <mohamed.amadou159@gmail.com>
2020-10-15 15:29:17 -0400

    Add scaffolding for projects and community involvement copy

---
+++ src/model/projectCopy.js
@@ -0,0 +1,31 @@
+const projects = {
+  complete: {
+    "Find It! Painless Shopping": {},
+    "White Lily Casino": {},
+    Mynute: {},
+    Speculator: {},
+  },
+  in_progress: {
+    Symphonia: {},
+    Jurisprudence: {},
+  },
+};
+
+const communityInvolvement = {
+  current: {
+    "Hack the North": {},
+    "Contrast: A Cappella": {},
+    "tech+: Mentorship": {},
+  },
+  past: {
+    "The Water Boys: A Cappella": {},
+    "JDCC: Extended Strategy": {},
+  },
+};
+
+const caseCompetitions = {
+  current: {},
+  past: {},
+};
+
+module.exports = { projects, communityInvolvement };
