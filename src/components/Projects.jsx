696a87156bed03c99aa17a3790197ed77a031d30
GitHub <mohamed.amadou159@gmail.com>
2021-03-16 12:02:16 -0400

    Include projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,7 @@ class Projects extends React.Component {
   render() {
     return (
       <div className="projects__container">
-        {/* Find It! Painless Shopping; The Water Boys Arrangements & Performances (Add my old facebook banner);
-         White Lily Casino; Mynute; Speculator; Hack the North; Contrast (Add performance reel); Symphonia (WIP)*/}
+        <div className="sr-oscillation rectangle" />
       </div>
     );
   }
